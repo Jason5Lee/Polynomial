c92027d94fcd74ecb7a7e6b50c361d6b11c7aeba
Jason5Lee <jason5lee@hotmail.com>
2024-02-10 12:31:23 +0800

    Refactor

--- include/polynomial.hpp
+++ include/polynomial.hpp
@@ -1,30 +1,29 @@
-#pragma once
+#ifndef JASON5LEE_POLYNOMIAL_POLYNOMIAL_H
+#define JASON5LEE_POLYNOMIAL_POLYNOMIAL_H
 
-#include<string>
-#include<initializer_list>
-#include<vector>
-#include<iostream>
-#include<stdexcept>
+#include <initializer_list>
+#include <iostream>
+#include <stdexcept>
+#include <vector>
 
 // A polynomial term
 class Term {
 public:
     // Exception when trying to construct an term with a negative degree
-    struct NegativeDegreeException: std::runtime_error {
-        NegativeDegreeException(): std::runtime_error("The degree cannot be negative") {}
+    struct NegativeDegreeException : std::runtime_error {
+        NegativeDegreeException() : std::runtime_error("The degree cannot be negative") {}
     };
 
-    Term(double coe = 0, int deg = 0)
-            : coefficient(coe), degree(deg) {
-        if (deg < 0)
-            throw NegativeDegreeException();
+    Term(double coe = 0, int deg = 0) : coefficient(coe), degree(deg) {
+        if (deg < 0) throw NegativeDegreeException();
     }
 
     // getters
     double getCoefficient() const { return coefficient; }
     int getDegree() const { return degree; }
 
-    // Determine whether two items are of the same type, and if so, combine the two items.
+    // Determine whether two items are of the same type, and if so, combine the
+    // two items.
     bool tryMergeWith(const Term &other);
 
     // Get the derivative of this term.
@@ -34,6 +33,7 @@ public:
 
     // Determine whether the coefficient is 0.
     bool useless() const;
+
 private:
     double coefficient;
     int degree;
@@ -41,11 +41,6 @@ private:
 
 class Polynomial {
 public:
-    // Exception when parsing failed
-    struct PolynomialFormatException: std::runtime_error {
-        PolynomialFormatException(): std::runtime_error("Invalid polynomial") {}
-    };
-
     // Constructs an empty polynomial, respresenting the constant 0.
     Polynomial() = default;
 
@@ -54,9 +49,6 @@ public:
 
     // Construct a general polynomial from an initialization list.
     Polynomial(std::initializer_list<Term>);
-
-    // Parses the polynomial.
-    static Polynomial parse(const std::string &);
 
     // Determine if it is an empty polynomial.
     bool empty() const;
@@ -69,21 +61,27 @@ public:
     friend Polynomial operator+(const Polynomial &, const Polynomial &);
     friend Polynomial operator-(const Polynomial &, const Polynomial &);
 
-    // Since constant (double value) can be implicitly converted to Polynomial with the constructor,
-    // extra multiplication operator for multiplying polynomials by constants is not needed.
+    // Since constant (double value) can be implicitly converted to Polynomial
+    // with the constructor, extra multiplication operator for multiplying
+    // polynomials by constants is not needed.
     friend Polynomial operator*(const Polynomial &, const Polynomial &);
 
     // Evaluation.
     double operator()(double x) const;
 
-    // Print the polynomial.
+    // Read the polynomial from the input stream.
+    friend std::istream &operator>>(std::istream &, Polynomial &);
+    // Output the polynomial to the output stream.
     friend std::ostream &operator<<(std::ostream &, const Polynomial &);
 
 private:
     std::vector<Term> poly;
     typedef decltype(poly.size()) size_t;
 
-    // Arrange up polynomials. 
-    // It sorts the terms in descending order of degree, merge the same-degree terms, and delete the terms with a coefficient of 0.
+    // Arrange up polynomials.
+    // It sorts the terms in descending order of degree, merge the same-degree
+    // terms, and delete the terms with a coefficient of 0.
     void arrange();
 };
+
+#endif  // JASON5LEE_POLYNOMIAL_POLYNOMIAL_H
